0a79b4d86d555bf73ee9f5b32981ea7a891b8bad
Harish Singh <harishsingh.hs.hb@gmail.com>
2021-05-27 17:02:18 +0530

    db config file update

--- ecommerce/databases/connection.py
+++ ecommerce/databases/connection.py
@@ -16,9 +16,9 @@ class DBConnection:
     def __connect(self):
         try:
             if self.config['DEFAULT']['ENV'] == 'local':
-                db = MongoClient(f"mongodb+srv://{self.config['LOCAL']['MONGO_USERNAME']}:{self.config['LOCAL']['MONGO_PASSWORD']}@cluster0.wdc0s.mongodb.net/{self.config['LOCAL']['DB_NAME']}?retryWrites=true&w=majority")
+                db = MongoClient(f"mongodb+srv://{self.config['LOCAL']['MONGO_USERNAME']}:{self.config['LOCAL']['MONGO_PASSWORD']}@{self.config['LOCAL']['MONGO_CLUSTER']}/{self.config['LOCAL']['DB_NAME']}?retryWrites=true&w=majority")
             else:
-                db = MongoClient(f"mongodb+srv://{self.config['PROD']['MONGO_USERNAME']}:{self.config['PROD']['MONGO_PASSWORD']}@cluster0.wdc0s.mongodb.net/{self.config['PROD']['DB_NAME']}?retryWrites=true&w=majority")
+                db = MongoClient(f"mongodb+srv://{self.config['PROD']['MONGO_USERNAME']}:{self.config['PROD']['MONGO_PASSWORD']}@{self.config['LOCAL']['MONGO_CLUSTER']}/{self.config['PROD']['DB_NAME']}?retryWrites=true&w=majority")
             return db
         except Exception as e:
             raise Exception('Error: ', e)
