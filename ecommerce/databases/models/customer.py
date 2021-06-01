d16df92385c1ac4942acbe225a89978fb414d0a5
Harish Singh <harishsingh.hs.hb@gmail.com>
2021-06-01 21:45:00 +0530

    update all files and folder from user to customer

--- ecommerce/databases/models/customer.py
+++ ecommerce/databases/models/customer.py
@@ -1,9 +1,9 @@
 from ecommerce.databases.models import Model
 
 
-class User(Model):
+class Customer(Model):
     def __init__(self):
-        super(User, self).__init__('Customer')
+        super(Customer, self).__init__('Customer')
         self.hidden_fields = ['password']
 
         # self.logPath = 'ecommerce/logs/customer'
